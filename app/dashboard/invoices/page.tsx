a98e5677de2611c83539556afede087843dc21ae
ortima <account.net@.mail.ru>
2024-04-28 15:34:04 +0300

    lesson-16 next-js

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,7 +6,11 @@ import { Suspense } from 'react';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 export default async function Page({
   searchParams,
 }: {
--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
+import { DeleteInvoice, UpdateInvoice } from '@/app/ui/invoices/buttons';
 import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
+import { formatCurrency, formatDateToLocal } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
 
 export default async function InvoicesTable({
